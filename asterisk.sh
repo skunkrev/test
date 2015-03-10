405a2dfc978e4e71225d9a507b1217668f4a553f
skunkrev <24.01.2k10@gmail.com>
2015-03-10 20:39:35 +0100

    Update asterisk.sh

--- asterisk.sh
+++ asterisk.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 echo "Script d'installation du serveur Asterisk"
 echo "Mettre a jour ?"
-
-select choix in Oui Non
-do case $choix in 
-# Laisser passer ceux qui répondent correctement à la question
-"Oui") apt-get update;;
-"Non") break;;
-esac        
-done
-
+read choix
+if $choix = "Oui"
+then apt-get update
+else break
+fi
