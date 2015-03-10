860959f511b4c336bb0a2d1a590bb752cff823e5
skunkrev <24.01.2k10@gmail.com>
2015-03-10 19:32:52 +0100

    Update asterisk.sh

--- asterisk.sh
+++ asterisk.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 echo "Script d'installation du serveur Asterisk"
-apt-get update
-
 echo "Mettre a jour ?"
-select opinion in Pour Contre
-do case $opinion in 
-# Laisser passer ceux qui répondent correctement à la question
-"Pour"|"Contre") break;;
-# Au cas où des zozos tapent sur autre chose que 1 ou 2
+select opinion in Oui Non
+do case $choix in 
+"Oui"|"Non") break;;
 "*") continue;;
 esac        
 done
 
-# M'envoyer le résultat par mail
-echo "$opinion" | mail bourdieu
+if $choix == "Oui"
+else apt-get update
+fi
